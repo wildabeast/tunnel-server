d20edbe0c428cfa0967f383b497c1f7d81debb9d
Ubuntu <ubuntu@ip-10-185-174-24.ec2.internal>
2014-08-14 01:28:54 +0000

    initial commit, brah

---
+++ app.js
@@ -0,0 +1,32 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+Error.stackTraceLimit = Infinity;
+
+var fs = require('fs');
+var Server = require('tls-tunnel').Server;
+
+var server = new Server({
+  port: 8080,   // port to listen for client connections
+  key: fs.readFileSync('./keys/server-key.pem'),    // server's private key
+  cert: fs.readFileSync('./keys/server-cert.pem'),  // server's SSL certificate
+  ca: [fs.readFileSync('./keys/client-cert.pem')],  // list of authorized client SSL certificates
+  forwardedPorts: {
+    start: 8081,    // Start of port range to assign to connecting clients
+    count: 10       // maximum number of ports and hence clients that can be supported
+  }
+});
+
+server.on('open', function(port) {
+	console.log('client connection opened on port ' + port + ".");
+});
+
+server.on('connect', function() {
+	console.log('client connected.');
+});
+
+server.start(function() {
+  // server should be listening on port 8080 now
+	console.log('server started.'); 
+// server.stop(function() {
+    // server should have ended all connections and stopped
+  //});
+});
